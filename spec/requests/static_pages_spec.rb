e6782d110d12f126300eab8cd895d45a716db2de
Vera Chan <vechan888@gmail.com>
2012-09-19 14:23:23 -0700

    updated rspec to add about section, added to routes.rb and static_pages_controller.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,5 +16,12 @@ describe "StaticPages" do
     end
   end
 
+    describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us') # note the content text is case-sensitive
+    end
+  end
+  
 end
 
