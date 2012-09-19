3d52c82e36b378c98947b903f2c31aa71f7ee956
Vera Chan <vechan888@gmail.com>
2012-09-19 14:12:52 -0700

    added Help page test on rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
+  
+  describe "Home page" do
+    it "should have the content 'SampleApp'" do
       visit '/static_pages/home'
       page.should have_content('SampleApp')
     end
   end
+  
+    describe "Help page" do
+    it "should have the content 'help'" do
+      visit '/static_pages/help'
+      page.should have_content('help') # note the content text is case-sensitive
+    end
+  end
+
 end
+
