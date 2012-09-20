33cf5f406460ea25db2a142b33394e0b446eed80
Vera Chan <vechan888@gmail.com>
2012-09-20 11:20:13 -0700

    Finish layout and routes for Signin section

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,47 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  
+describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Sample App' in h1" do
-      visit '/static_pages/home'
-      # page.should have_content('SampleApp')
-      page.should have_selector('h1', :text => 'Sample App')
+    before { visit root_path }
 
-    end
-    
-        it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Home")
-    end
-  end
- 
-    describe "Help page" do
-    it "should have the content 'help'" do
-      visit '/static_pages/help'
-      # page.should have_content('help') # note the content text is case-sensitive
-      page.should have_selector('h1', :text => 'Help')
-    end
-    
-        it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Help")
-    end
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
+  describe "Help page" do
+    before { visit help_path }
 
-    describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      # page.should have_content('About Us') # note the content text is case-sensitive
-      page.should have_selector('h1', :text => 'About Us')
-    end
-    
-        it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "About")
-    end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
-  
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
 end
-
